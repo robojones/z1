02eef8542f8ddac0de960d19300c77261e1c1875
robojones <jonathan.scholz-weil@t-online.de>
2017-02-06 10:00:02 +0100

    add integration test

--- test/remote/index.js
+++ test/remote/index.js
@@ -4,24 +4,24 @@ describe('remote', function () {
 
   const remote = local('remote/index')
   const Remote = local('remote/class/Remote')
+  const exampleServer = local.resolve('example')
 
   it('should export an instance of Remote', function () {
     assert(remote instanceof Remote)
   })
 
-  beforeEach(function () {
-    //return remote.exit()
+  afterEach(function () {
+    return remote.exit().then(() => log('should be killed'))
   })
 
   describe('.ping & .connect', function () {
     it('should be rejected if the daemon is not started', function (cb) {
       remote.exit().then(() => {
-        return remote.ping()
-          .then(() => {
-            cb(new Error('how could this ever resolve??'))
-          }).catch(err => {
-            cb()
-          })
+        return remote.ping().then(() => {
+          cb(new Error('how could this ever resolve??'))
+        }).catch(err => {
+          cb()
+        })
       })
     })
 
@@ -30,5 +30,25 @@ describe('remote', function () {
         return remote.ping()
       })
     })
+  })
+
+  describe('start-restart-stop cycle', function () {
+    return remote.start(exampleServer).then(data => {
+      return remote.list().then(list => {
+        assert(list[data.app], 'app not in the list')
+        assert.strictEqual(list[data.app].available, data.started, 'wrong number of workers')
+        return remote.restart(data.app, 100)
+      })
+    }).then(data => {
+      return remote.list().then(list => {
+        assert(list[data.app], 'app not in the list')
+        assert.strictEqual(list[data.app].available, data.started, 'wrong number of workers')
+        return remote.stop(data.app, 100)
+      })
+    }).then(data => {
+      return remote.list().then(list => {
+        assert(!Object.keys(list).length, 'there are workers left in the list')
+      })
+    }).catch(err => console.log(err))
   })
 })
