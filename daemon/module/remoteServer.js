45980a84ab1121b689f1e1df31c80e39e37df4e7
robojones <jonathan.scholz-weil@t-online.de>
2017-09-03 15:03:19 +0200

    add JSDoc to remoteServer.js

--- daemon/module/remoteServer.js
+++ daemon/module/remoteServer.js
@@ -7,6 +7,11 @@ const OPT = {
   allowHalfOpen: true
 }
 
+/**
+ * Creates a UNIX server to communicate with the z1 API.
+ * @param {string} filename - The name for the UNIX socket.
+ * @param {function} run - A function that gets called every time an object is received.
+ */
 function remoteServer(filename, run) {
   const file = path.resolve(filename)
 
