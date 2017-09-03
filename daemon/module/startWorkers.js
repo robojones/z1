6e46f6b71d57000903b49211cbf7dd955abac581
robojones <jonathan.scholz-weil@t-online.de>
2017-09-03 16:40:38 +0200

    use a/a in startWorkers.js

--- daemon/module/startWorkers.js
+++ daemon/module/startWorkers.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const cpuCount = require('os').cpus().length
 const cluster = require('cluster')
-const mkdirp = require('mkdirp')
+const mkdirp = require('mkdirp-promise')
 
 const Worker = require('./../class/Worker')
 const logs = require('./log')
@@ -11,123 +11,116 @@ const NOEND = {
   end: false
 }
 
-module.exports = function startWorkers(config, dir, pack, args = [], env = {}) {
+module.exports = async function startWorkers(config, dir, pack, args = [], env = {}) {
   const workers = []
 
-  return new Promise((resolve, reject) => {
-    if (pack.name === 'z1') {
-      throw new Error('the name "z1" is invalid')
-    }
+  if (pack.name === 'z1') {
+    throw new Error('the name "z1" is invalid')
+  }
 
-    const ports = pack.ports
-    const out = logs.get(pack.name)
+  const ports = pack.ports
+  const out = logs.get(pack.name)
 
-    // output path
-    let output = null
-    if (typeof pack.output === 'string') {
-      if (path.isAbsolute(pack.output)) {
-        output = pack.output
-      } else {
-        output = path.join(dir, pack.output)
-      }
+  // output path
+  let output = null
+  if (typeof pack.output === 'string') {
+    if (path.isAbsolute(pack.output)) {
+      output = pack.output
     } else {
-      output = path.join(process.env.HOME, '.z1', pack.name)
+      output = path.join(dir, pack.output)
     }
+  } else {
+    output = path.join(process.env.HOME, '.z1', pack.name)
+  }
 
-    const workerCount = Math.abs(+pack.workers) || cpuCount
+  const workerCount = Math.abs(+pack.workers) || cpuCount
 
-    const e = []
-    const q = []
-    const n = []
+  const exitPromises = []
+  const availablePromises = []
+  const n = []
 
-    // create output dir
-    n.push(new Promise((resolve, reject) => {
-      mkdirp(output, err => {
-        if (err && err.code !== 'EEXIST') {
-          reject(err)
+  // setup logs
+  await mkdirp(output)
+
+  logs.setup(pack.name, output)
+
+  // setup master
+  cluster.setupMaster({
+    stdio: ['ignore', 'pipe', 'pipe', 'ipc'],
+    args
+  })
+
+  const ENV = Object.assign({}, env, {
+    PWD: dir,
+    APPNAME: pack.name,
+    PORT: ports[0],
+    PORTS: ports.join()
+  })
+
+  for (let i = 0; i < workerCount; i += 1) {
+    const worker = new Worker(dir, pack.main, pack.name, ports, ENV)
+    const w = worker.w
+    w.process.stdout.pipe(out.log, NOEND)
+    w.process.stderr.pipe(out.err, NOEND)
+    w.on('error', handle)
+
+    workers.push(worker)
+
+    exitPromises.push(worker.once('exit').then(code => {
+      throw new Error(`worker of app "${pack.name}" not started (exit code: ${code})`)
+    }))
+    availablePromises.push(worker.once('available'))
+
+    worker.once('available').then(async () => {
+      const code = await worker.once('exit')
+      if (code) {
+        log(`worker ${worker.id} of "${worker.name}" crashed. (code: ${code})`)
+      }
+
+      if (worker.state !== Worker.KILLED) {
+        // revive worker
+        log(`starting 1 new worker for "${worker.name}"`)
+
+        const app = config.apps.find(app => app.name === worker.name)
+
+        log('found app', app.name)
+
+        if (!app) {
           return
         }
 
-        logs.setup(pack.name, output)
+        if (!app.reviveCount) {
+          app.reviveCount = 0
+        }
 
-        resolve()
-      })
-    }))
+        app.reviveCount += 1
 
-    // setup master
-    cluster.setupMaster({
-      stdio: ['ignore', 'pipe', 'pipe', 'ipc'],
-      args
-    })
-
-    const ENV = Object.assign({}, env, {
-      PWD: dir,
-      APPNAME: pack.name,
-      PORT: ports[0],
-      PORTS: ports.join()
-    })
-
-    for (let i = 0; i < workerCount; i++) {
-      const worker = new Worker(dir, pack.main, pack.name, ports, ENV)
-      const w = worker.w
-      w.process.stdout.pipe(out.log, NOEND)
-      w.process.stderr.pipe(out.err, NOEND)
-      w.on('error', handle)
-
-      workers.push(worker)
-
-      e.push(worker.once('exit').then(code => {
-        throw new Error(`worker of app "${pack.name}" not started (exit code: ${code})`)
-      }))
-      q.push(worker.once('available').then(() => {
-        worker.once('exit', code => {
-          if (code) {
-            log(`worker ${worker.id} of "${worker.name}" crashed. (code: ${code})`)
-          }
-
-          if (worker.state !== Worker.KILLED) {
-            // revive worker
-            log(`starting 1 new worker for "${worker.name}"`)
-
-            const app = config.apps.find(app => app.name === worker.name)
-
-            log('found app', app.name)
-
-            if (!app) {
-              return
-            }
-
-            if (!app.reviveCount) {
-              app.reviveCount = 0
-            }
-
-            app.reviveCount += 1
-
-            const pkg = Object.assign({}, pack, {
-              workers: 1
-            })
-
-            startWorkers(config, dir, pkg, args, env).catch(handle)
-          }
+        const pkg = Object.assign({}, pack, {
+          workers: 1
         })
-      }))
-      log('listening for exit')
-    }
 
-    // when all workers are online before an error
-    e.push(Promise.all(q))
-    n.push(Promise.race(e))
+        await startWorkers(config, dir, pkg, args, env)
+      }
+    }).catch(handle)
+  }
 
-    Promise.all(n).then(() => {
-      resolve({
-        app: pack.name,
-        dir,
-        started: workerCount,
-        ports
-      })
-    }).catch(err => {
-      // if one worker crashes => kill all workers
-      killWorkers(workers, 0).then(() => reject(err))
-    })
-  })
+  const availablePromise = Promise.all(availablePromises)
+  const exitPromise = Promise.race(exitPromises)
+
+  try {
+    // Wait for all workers to start.
+    await Promise.race([exitPromise, availablePromise])
+  } catch (err) {
+    // if one worker crashes => kill all workers
+    await killWorkers(workers, 0)
+
+    throw err
+  }
+
+  return {
+    app: pack.name,
+    dir,
+    started: workerCount,
+    ports
+  }
 }
