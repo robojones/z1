ab082731c188269291fa9d425812da77ed5e64e9
robojones <jonathan.scholz-weil@t-online.de>
2017-09-03 10:13:54 +0200

    update test command

--- test/index.js
+++ test/index.js
@@ -1,13 +1,28 @@
-const fs = require('mz/fs')
-const path = require('path')
+const z1 = require('..')
+const { once } = require('better-events')
+const { spawn } = require('child_process')
+let daemon
 
 before(async function () {
-  console.log('remove config.json.')
-  try {
-    await fs.unlink(path.resolve(process.env.HOME, '.z1/config.json'))
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      throw err
-    }
-  }
+  // wait for the test-daemon to start
+
+  this.timeout(10000)
+  daemon = spawn('istanbul', ['cover', './daemon/main.js', '--dir', './coverage/daemon'], {
+    stdio: 'inherit'
+  })
+
+  const connection = z1._waitForConnection()
+  const error = once(daemon, 'error')
+
+  await Promise.race([connection, error])
+})
+
+after(async function () {
+  // wait for daemon to stop
+
+  this.timeout(10000)
+
+  z1.exit()
+
+  await once(daemon, 'exit')
 })
