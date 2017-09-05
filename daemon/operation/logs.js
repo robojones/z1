b15d0fad42dbeffe382c0f5c9ce4858df7c7b9ce
robojones <jonathan.scholz-weil@t-online.de>
2017-09-05 11:47:07 +0200

    z1 log now outputs stderr and stdout separated

--- daemon/operation/logs.js
+++ daemon/operation/logs.js
@@ -2,16 +2,17 @@ const log = require('../lib/log')
 
 async function logs(config, command, connection) {
   const streams = log.get(command.app)
-  const logString = msg => connection.log(msg.toString())
+  const sendOutToCLI = connection.stdout.bind(connection)
+  const sendErrToCLI = connection.stderr.bind(connection)
 
-  streams.log.on('data', logString)
-  streams.err.on('data', logString)
+  streams.log.on('data', sendOutToCLI)
+  streams.err.on('data', sendErrToCLI)
 
   return new Promise(resolve => {
     connection.on('message', msg => {
       if (msg.type === 'SIGINT') {
-        streams.log.removeListener('data', logString)
-        streams.err.removeListener('data', logString)
+        streams.log.removeListener('data', sendOutToCLI)
+        streams.err.removeListener('data', sendErrToCLI)
         resolve({})
       }
     })
