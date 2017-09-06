e21b40fb8d41657cba15a91323b957d41e176fde
robojones <jonathan.scholz-weil@t-online.de>
2017-09-06 10:12:15 +0200

    use a/a and the new killWorkers method in restart.js

--- daemon/operation/restart.js
+++ daemon/operation/restart.js
@@ -1,5 +1,6 @@
 const Worker = require('../lib/class/Worker')
 const startWorkers = require('../lib/startWorkers')
+const killWorkers = require('../lib/killWorkers')
 const getPack = require('../lib/getPack')
 
 /*
@@ -12,71 +13,66 @@ command {
 }
 */
 
-module.exports = function restart(config, command, connection) {
-  return new Promise((resolve, reject) => {
-    if (global.isResurrectable) {
-      throw new Error('no apps running')
+async function restart(config, command, connection) {
+  if (global.isResurrectable) {
+    throw new Error('no apps running')
+  }
+
+  // find old app
+  const app = config.apps.find(app => app.name === command.app)
+
+  if (!app) {
+    throw new Error(`app "${command.app}" not found`)
+  }
+
+  // reload package.json
+  const pack = getPack(app.dir, app.opt, app.env)
+
+  // if name changed
+  const nameChanged = pack.name !== app.name
+  if (nameChanged) {
+    // check name
+    if (config.apps.some(app => app.name === pack.name)) {
+      throw new Error(`new name "${pack.name}" already in use`)
     }
 
-    // find old app
-    const app = config.apps.find(app => app.name === command.app)
+    // save new app
+    config.apps.push(Object.assign({}, app, {
+      name: pack.name
+    }))
 
-    if (!app) {
-      throw new Error(`app "${command.app}" not found`)
+    config.save()
+  }
+
+  // set default timeout
+  let timeout = (app.env.NODE_ENV === 'development') ? 0 : 30e3
+
+  if (command.opt.timeout) {
+    if (isNaN(+command.opt.timeout)) {
+      timeout = null
+    } else {
+      timeout = +command.opt.timeout
     }
+  }
 
-    // reload package.json
-    const pack = getPack(app.dir, app.opt, app.env)
+  // remember old workers
+  const workers = Worker.workerList.filter(worker => worker.name === command.app)
 
-    // if name changed
-    const nameChanged = pack.name !== app.name
-    if (nameChanged) {
-      // check name
-      if (config.apps.some(app => app.name === pack.name)) {
-        throw new Error(`new name "${pack.name}" already in use`)
-      }
+  const data = await startWorkers(config, app.dir, pack, app.args, app.env, connection)
 
-      // save new app
-      config.apps.push(Object.assign({}, app, {
-        name: pack.name
-      }))
+  // kill old workers
+  const workersKilled = await killWorkers(workers, timeout, command.opt.signal)
 
-      config.save()
-    }
+  if (nameChanged) {
+    // remove old version
+    const oldIndex = config.apps.findIndex(app => app.name === command.app)
+    config.apps.splice(oldIndex, 1)
+    config.save()
+  }
 
-    // set default timeout
-    let timeout = (app.env.NODE_ENV === 'development') ? 0 : 30e3
+  data.killed = workersKilled
 
-    if (command.opt.timeout) {
-      if (isNaN(+command.opt.timeout)) {
-        timeout = null
-      } else {
-        timeout = +command.opt.timeout
-      }
-    }
-
-    // remember old workers
-    const workers = Worker.workerList.filter(worker => worker.name === command.app)
-
-    startWorkers(config, app.dir, pack, app.args, app.env, connection).then(data => {
-      // kill old workers
-      const killed = workers.map(worker => {
-        if (worker.kill(command.opt.signal, timeout)) {
-          return worker.once('exit')
-        }
-      })
-
-      return Promise.all(killed).then(() => {
-        if (nameChanged) {
-          // remove old version
-          const oldIndex = config.apps.findIndex(app => app.name === command.app)
-          config.apps.splice(oldIndex, 1)
-          config.save()
-        }
-
-        data.killed = killed.length
-        resolve(data)
-      })
-    }).catch(reject)
-  })
+  return data
 }
+
+module.exports = restart
