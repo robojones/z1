bfd3fed1938bc24e32124e04d6b82ab9326263d5
robojones <jonathan.scholz-weil@t-online.de>
2017-09-06 21:45:11 +0200

    exit daemon if dependency not found

--- daemon/lib/log.js
+++ daemon/lib/log.js
@@ -19,6 +19,10 @@ const z1Logs = log.setup('z1', 'z1')
 global.handle = err => {
   console.error(err)
   z1Logs.err.write(util.inspect(err) + '\n')
+
+  if (err.code === 'MODULE_NOT_FOUND') {
+    throw err
+  }
 }
 
 global.log = (...stuff) => {
