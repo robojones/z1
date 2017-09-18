2a87f802917ad31c938982f1bb67607ba5034b9f
robojones <jonathan.scholz-weil@t-online.de>
2017-09-18 09:35:04 +0200

    dim the white heading background

--- cli/lib/heading.js
+++ cli/lib/heading.js
@@ -4,7 +4,7 @@ const colors = require('colors/safe')
  * Logs a horizontal line with the width of the terminal.
  */
 function heading(msg = '') {
-  console.log(colors.inverse(msg + ' '.repeat(process.stdout.columns - msg.length)))
+  console.log(colors.inverse.dim(msg + ' '.repeat(process.stdout.columns - msg.length)))
 }
 
 module.exports = heading
