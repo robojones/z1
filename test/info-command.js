18d70ee9f03026a0435d85dbd299f3d9e7cae3fd
robojones <contact@jonathan-scholz.de>
2017-09-24 20:33:34 +0200

    test exit code of info command

--- test/info-command.js
+++ test/info-command.js
@@ -11,19 +11,23 @@ const {
 describe('info command', function () {
   this.timeout(TIMEOUT)
 
+  before(async function () {
+    await z1.start('test-app/basic', [], {
+      workers: 1
+    })
+  })
+
+  after(async function () {
+    await z1.stop('basic', {
+      timeout: KILL_TIMEOUT
+    })
+  })
+
+  it('should not exit with an exit code', async function () {
+    await works('z1 info basic')
+  })
+
   describe('option', function () {
-    before(async function () {
-      await z1.start('test-app/basic', [], {
-        workers: 1
-      })
-    })
-
-    after(async function () {
-      await z1.stop('basic', {
-        timeout: KILL_TIMEOUT
-      })
-    })
-
     /**
      * Test if the given option outputs the expected result
      * @param {string} optionName 
@@ -31,19 +35,19 @@ describe('info command', function () {
      */
     function checkOption(optionName, expectedResult) {
       describe(optionName, function () {
-        it('should output the directory of the app', async function () {
-          const result = await works('z1 info basic ' + optionName)
+        it(`should output the ${optionName} of the app`, async function () {
+          const result = await works(`z1 info basic --${optionName}`)
           assert.strictEqual(result.out, expectedResult + '\n')
         })
       })
     }
 
-    checkOption('--available', '1')
-    checkOption('--dir', path.resolve('test-app/basic'))
-    checkOption('--killed', '0')
-    checkOption('--name', 'basic')
-    checkOption('--pending', '0')
-    checkOption('--ports', '8080')
-    checkOption('--revive-count', '0')
+    checkOption('available', '1')
+    checkOption('dir', path.resolve('test-app/basic'))
+    checkOption('killed', '0')
+    checkOption('name', 'basic')
+    checkOption('pending', '0')
+    checkOption('ports', '8080')
+    checkOption('revive-count', '0')
   })
 })
