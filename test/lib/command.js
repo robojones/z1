e86cb35a73e5a6dc75ae0e76ec488ed21b8dc290
robojones <contact@jonathan-scholz.de>
2017-09-24 02:29:46 +0200

    remove istanbul from custom command-test

--- test/lib/command.js
+++ test/lib/command.js
@@ -1,29 +1,9 @@
 const command = require('command-test')
 const path = require('path')
-const dirList = []
 const cliFile = path.resolve('cli/main.js')
 
-function getDir(cmd) {
-  let dir = path.join('coverage', cmd.replace(/\W+/g, '-'))
-  let no = 0
-
-  while (dirList.includes(dir)) {
-    const i = dir.indexOf('.')
-    if (i !== -1) {
-      dir = dir.substr(0, i)
-    }
-
-    no += 1
-    dir += '.' + no
-  }
-
-  dirList.push(dir)
-  return dir
-}
-
 function modify(cmd) {
-  const dir = getDir(cmd)
-  return cmd.replace(/z1/, `istanbul cover --dir ${dir} ${cliFile} --`)
+  return cmd.replace(/z1/, `${cliFile}`)
 }
 
 /**
