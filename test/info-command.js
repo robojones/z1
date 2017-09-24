3762f994252cbb048196a7fa0d4c9132cbc3f5b8
robojones <contact@jonathan-scholz.de>
2017-09-24 20:24:51 +0200

    add tests for info-command

---
+++ test/info-command.js
@@ -0,0 +1,46 @@
+const assert = require('assert')
+const path = require('path')
+
+const z1 = require('..')
+const { works } = require('./lib/command')
+const { TIMEOUT } = require('./lib/config')
+
+describe('info command', function () {
+  this.timeout(TIMEOUT)
+
+  describe('option', function () {
+    before(async function () {
+      await z1.start('test-app/basic', [], {
+        workers: 1
+      })
+    })
+
+    after(async function () {
+      await z1.stop('basic', {
+        timeout: 10000
+      })
+    })
+
+    /**
+     * Test if the given option outputs the expected result
+     * @param {string} optionName 
+     * @param {string} expectedResult 
+     */
+    function checkOption(optionName, expectedResult) {
+      describe(optionName, function () {
+        it('should output the directory of the app', async function () {
+          const result = await works('z1 info basic ' + optionName)
+          assert.strictEqual(result.out, expectedResult + '\n')
+        })
+      })
+    }
+
+    checkOption('--available', '1')
+    checkOption('--dir', path.resolve('test-app/basic'))
+    checkOption('--killed', '0')
+    checkOption('--name', 'basic')
+    checkOption('--pending', '0')
+    checkOption('--ports', '8080')
+    checkOption('--revive-count', '0')
+  })
+})
